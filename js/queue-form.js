16db8eba99566c5ec36a28598e0243faf5ac8f4b
rubicae <git@tegitur.com>
2019-12-21 23:12:14 -0800

    Remove dependence on WHATWG URL for IE11

--- js/queue-form.js
+++ js/queue-form.js
@@ -37,9 +37,36 @@ $(function() {
   });
 });
 
+function extractSearchParam(key) {
+  var url = window.location.href;
+  var qs = "";
+  var afterKey = "";
+
+  if (url.indexOf("?") < 0) {
+    return "";
+  }
+
+  qs = url.substring(url.indexOf("?") + 1);
+  if (qs.indexOf("#") >= 0) {
+    qs = qs.substring(0, qs.indexOf("#"));
+  }
+
+  if (qs.substring(0, key.length + 1) === (key + "=")) {
+    afterKey = qs.substring(key.length + 1);
+  } else if (qs.indexOf("&" + key + "=") >= 0) {
+    afterKey = qs.substring(qs.indexOf("&" + key + "=") + key.length + 2);
+  } else {
+    return "";
+  }
+
+  if (afterKey.indexOf("&") >= 0) {
+    return afterKey.substring(0, afterKey.indexOf("&"));
+  }
+  return afterKey;
+}
+
 function init_form() {
-  var url = new URL(window.location.href);
-  var domain = url.searchParams.get('domain');
+  var domain = extractSearchParam('domain');
   if (domain) {
     $('#domain-input').val(domain);
     $('.your-domain').text(domain);
@@ -47,7 +74,7 @@ function init_form() {
 
   init_toggle_mta_sts();
   init_add_another_mx();
-  var mta_sts = url.searchParams.get('mta_sts');
+  var mta_sts = extractSearchParam('mta_sts');
   if (mta_sts == 'true') {
     $('#mta-sts-input').prop('checked', true);
     $('#mx-hostnames').hide();
